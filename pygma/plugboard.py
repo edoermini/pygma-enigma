e94712905fd2b0ba95b43de62e00f2ec3cfb9309
Edoardo Ermini <33008912+MrRadix@users.noreply.github.com>
2017-11-02 22:18:12 +0100

    Update plugboard.py

--- pygma/plugboard.py
+++ pygma/plugboard.py
@@ -1,47 +1,47 @@
 def PlugBoard(words):
-	stecker = {}
+	pb = {}
 	count = 0
-	steckcheck = []
+	pb_check = []
 
 	words = words.lower()
 
 	if words == "":
-		stecker = {}
+		pb = {}
 		return None
 
-	steck = words.replace(" ", "")
-	steck = steck.split(",")
+	pb_list = words.replace(" ", "")
+	pb_list = pb_list.split(",")
 
-	for i in steck:
+	for i in pb_list:
 		if len(i) > 2 or len(i) < 2:
 			raise ValueError("Comma-separated groups of two letters are required")
 
-	for i in steck:
+	for i in pb_list:
 		for j in i:
 			if j.isdigit() == True:
 				raise TypeError("Unsupported numbers")
 
-	for i in steck:
-		steckcheck.append(i)
+	for i in pb_list:
+		pb_check.append(i)
 
-	for i in steckcheck:
-		del steckcheck[count]
+	for i in pb_check:
+		del pb_check[count]
 		
-		for j in steckcheck:
+		for j in pb_check:
 			if i[0] == j[0] or i[1] == j[1] or i[0] == j[1] or i[1] == j[0]:
 				raise ValueError("Same element found several times")
 				return None
 
 		count += 1
 
-		del steckcheck[:]
-		for i in steck:
-			steckcheck.append(i)
+		del pb_check[:]
+		for i in pb_list:
+			pb_check.append(i)
 
-	if len(steck) > 10:
+	if len(pb_list) > 10:
 		raise ValueError("Can not do more than 10 links")
 
-	for i in steck:
-		stecker.update({i[0]:i[1], i[1]:i[0]})
+	for i in pb_list:
+		pb.update({i[0]:i[1], i[1]:i[0]})
 
-	return stecker
+	return pb

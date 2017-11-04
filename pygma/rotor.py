abc9a634e3656fb7165505803f920449d418a980
Edoardo Ermini <33008912+MrRadix@users.noreply.github.com>
2017-11-04 17:28:46 +0100

    Update rotor.py

--- pygma/rotor.py
+++ pygma/rotor.py
@@ -1,28 +1,32 @@
-def Rotor(letters, position):
-	rotor = []
-	alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-	letters = letters.lower()
+class Rotor:
 	
-	if position < 1 or position > 26:
-		raise ValueError("A number between 1 and 26 with 1 and 26 included is required")
+	def __init__(self, letters, position):
+		self.rotor = []
+		self.letters = letters.lower()
+		self.position = position
+		
+		self.alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+		
+		#check if a letter between the alphabet has been chosen
+		if self.position < 1 or self.position > 26:
+			raise ValueError("A number between 1 and 26 with 1 and 26 included is required")
 
-	if len(letters) > 26 or len(letters) < 26:
-		raise ValueError("26 letters are required")
+		#check if all the alphabet has been writed
+		if len(self.letters) > 26 or len(self.letters) < 26:
+			raise ValueError("26 letters are required")
 
-	for index,letter in enumerate(letters):
-		for letter2 in letters[0:index] + letters[index+1:]:
-			if letter == letter2:
-				raise ValueError("Same element found several times")
+		for index,letter in enumerate(self.letters):
+			for letter2 in self.letters[0:index] + self.letters[index+1:]:
+				if letter == letter2:
+					raise ValueError("Same element found several times")
 
-	for index,letter in enumerate(letters):
-		if letter.isalpha() == True:
-				rotor.append(alphabet[index] + letter)
-		else:
-			raise TypeError("Unsupported element")
+		for index,letter in enumerate(self.letters):
+			if letter.isalpha() == True:
+					self.rotor.append(self.alphabet[index] + letter)
+			else:
+				raise TypeError("Unsupported element")
 
-	word = rotor[position-1]
-	index = rotor.index(word)
-	rotor = rotor[index:] + rotor[:index]
-
-	return rotor
+		word = self.rotor[position-1]
+		index = self.rotor.index(word)
+		
+		self.rotor = self.rotor[index:] + self.rotor[:index]

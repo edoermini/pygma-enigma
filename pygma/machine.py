f5fd1de17ad20bf8ff4e0566b8589e1c41fd7215
Edoardo Ermini <33008912+MrRadix@users.noreply.github.com>
2017-11-04 17:29:51 +0100

    Update machine.py

--- pygma/machine.py
+++ pygma/machine.py
@@ -1,49 +1,52 @@
+from .rotor import Rotor
+from .reflector import Reflector
+from .plugboard import Plugboard
+
 class Machine():
 	
-	def __init__(self, rotor, reflector, stecker):
+	def __init__(self, rotor, reflector, plugboard):
 		self.alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-		self.rotor_1 = rotor[0]
+		self.rotor_1 = rotor[0].rotor
 		self.rotor_1_left = []
 		self.rotor_1_right = []
 
-		self.rotor_2 = rotor[1]
+		self.rotor_2 = rotor[1].rotor
 		self.rotor_2_left = []
 		self.rotor_2_right = []
 
-		self.rotor_3 = rotor[2]
+		self.rotor_3 = rotor[2].rotor
 		self.rotor_3_left = []
 		self.rotor_3_right = []
 
-		self.reflector = reflector
+		self.reflector = reflector.reflector
 		self.reflector_left = []
 		self.reflector_right = []
 
-		self.stecker = stecker
+		self.plugboard = plugboard.plugboard
 
 		for i in self.rotor_1:
-			self.rotor_1_left.append(i[0])
-			self.rotor_1_right.append(i[1])
+			self.rotor_1_right.append(i[0])
+			self.rotor_1_left.append(i[1])
 
 		for i in self.rotor_2:
-			self.rotor_2_left.append(i[0])
-			self.rotor_2_right.append(i[1])
+			self.rotor_2_right.append(i[0])
+			self.rotor_2_left.append(i[1])
 
 		for i in self.rotor_3:
-			self.rotor_3_left.append(i[0])
-			self.rotor_3_right.append(i[1])
+			self.rotor_3_right.append(i[0])
+			self.rotor_3_left.append(i[1])
 
 		for i in self.reflector:
-			self.reflector_left.append(i[0])
-			self.reflector_right.append(i[1])
-
-	
-	def stecker_check(self, word):
+			self.reflector_right.append(i[0])
+			self.reflector_left.append(i[1])
+			
+	def plugboard_check(self, word):
 		changed = ""
 		
 		for i in word:
 			try:
-				ch = self.stecker[i.lower()]
+				ch = self.plugboard[i.lower()]
 				if i == i.lower():
 					changed += ch
 				elif i == i.upper():
@@ -55,13 +58,12 @@ class Machine():
 					changed += i.upper()
 
 		return changed
-
 	
 	def enc_dec(self, word):
 		Word = ""
-		rotor_1_count = 0
-		rotor_2_count = 0
-		rotor_3_count = 0
+		rotor_1_rotations = 0
+		rotor_2_rotations = 0
+		rotor_3_rotations = 0
 		rotor_1_left_mv = self.rotor_1_left
 		rotor_1_right_mv = self.rotor_1_right
 		rotor_2_left_mv = self.rotor_2_left
@@ -72,33 +74,33 @@ class Machine():
 		reflector_right_mv = self.reflector_right
 
 		for i in word:
-			rotor_1_count += 1
+			rotor_1_rotations += 1
 
-			i = self.stecker_check(i)
+			i = self.plugboard_check(i)
 				
 			try:
 				index = self.alphabet.index(i.lower())
 
-				word = rotor_1_left_mv[index]
-				index = rotor_1_right_mv.index(word)
-
-				word = rotor_2_left_mv[index]
-				index = rotor_2_right_mv.index(word)
-
-				word = rotor_3_left_mv[index]
-				index = rotor_3_right_mv.index(word)
-
-				word = reflector_left_mv[index]
-				index = reflector_right_mv.index(word)
-
-				word = rotor_3_right_mv[index]
-				index = rotor_3_left_mv.index(word)
+				word = rotor_1_right_mv[index]
+				index = rotor_1_left_mv.index(word)
 
 				word = rotor_2_right_mv[index]
 				index = rotor_2_left_mv.index(word)
 
-				word = rotor_1_right_mv[index]
-				index = rotor_1_left_mv.index(word)
+				word = rotor_3_right_mv[index]
+				index = rotor_3_left_mv.index(word)
+
+				word = reflector_right_mv[index]
+				index = reflector_left_mv.index(word)
+
+				word = rotor_3_left_mv[index]
+				index = rotor_3_right_mv.index(word)
+
+				word = rotor_2_left_mv[index]
+				index = rotor_2_right_mv.index(word)
+
+				word = rotor_1_left_mv[index]
+				index = rotor_1_right_mv.index(word)
 				
 				if i == i.upper():
 					Word += self.alphabet[index].upper()
@@ -108,28 +110,28 @@ class Machine():
 				rotor_1_left_mv = rotor_1_left_mv[1:] + rotor_1_left_mv[:1]
 				rotor_1_right_mv = rotor_1_right_mv[1:] + rotor_1_right_mv[:1]
 
-				if rotor_1_count == 26:
+				if rotor_1_rotations == 26:
 					rotor_2_left_mv = rotor_2_left_mv[1:] + rotor_2_left_mv[:1]
 					rotor_2_right_mv = rotor_2_right_mv[1:] + rotor_2_right_mv[:1]
 					
-					rotor_2_count += 1
-					rotor_1_count = 0
+					rotor_2_rotations += 1
+					rotor_1_rotations = 0
 
-				elif rotor_2_count == 26:
+				elif rotor_2_rotations == 26:
 					rotor_3_left_mv = rotor_3_left_mv[1:] + rotor_3_left_mv[:1]
 					rotor_3_right_mv = rotor_3_right_mv[1:] + rotor_3_right_mv[:1]
 
-					rotor_3_count +=1
-					rotor_2_count = 0
-					rotor_1_count = 0
+					rotor_3_rotations +=1
+					rotor_2_rotations = 0
+					rotor_1_rotations = 0
 
-				elif rotor_3_count == 26:
-					rotor_3_count = 0
-					rotor_2_count = 0
-					rotor_1_count = 0
+				elif rotor_3_rotations == 26:
+					rotor_3_rotations = 0
+					rotor_2_rotations = 0
+					rotor_1_rotations = 0
 			
 			except ValueError:
 				Word += i
 
-		Word = self.stecker_check(Word)		
+		Word = self.plugboard_check(Word)		
 		return Word
